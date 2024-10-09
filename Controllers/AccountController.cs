aa7f04f0bf516edab79bbe56c873c53573c70e21
Asyasimarmata <dindasimarmata@gmail.com>
2024-10-10 03:06:13 +0700

    Menambahkan fitur baru untuk tugas 2

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,103 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using SampleSecuredWeb.Data;
+using SampleSecuredWeb.Models;
+using SampleSecuredWeb.ViewModel;
+
+
+namespace SampleSecuredWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUser _userData;
+
+        public AccountController(IUser user)
+        {
+            _userData = user;
+        }
+        // GET: AccountController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(RegistrationViewModel registrationViewModel)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    var user = new Models.User
+                    {
+                        Username = registrationViewModel.Username,
+                        Password = registrationViewModel.Password,
+                        RoleName = "contributor"
+                    };
+                _userData.Registration(user);
+                return RedirectToAction("Index", "Home");
+                }
+            }
+            catch (System.Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                
+            }
+            return View(registrationViewModel);
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginViewModel loginViewModel)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    var user = new User
+                    {
+                        Username = loginViewModel.Username,
+                        Password = loginViewModel.Password
+                    };
+
+                    var loginUser = _userData.Login(user);
+
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, user.Username)
+                    };
+                    var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    var principal = new ClaimsPrincipal(identity);
+
+                    await HttpContext.SignInAsync(
+                        CookieAuthenticationDefaults.AuthenticationScheme,
+                        principal,
+                        new AuthenticationProperties
+                        {
+                            IsPersistent = loginViewModel.RememberLogin
+                        }
+                    );
+                   
+                        return RedirectToAction("Index", "Home");
+                   
+                }
+            }
+            catch (System.Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                
+            }
+            return View(loginViewModel);
+        }
+
+    }
+}
